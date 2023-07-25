f4599ea81fcc48417e5789999f16160b9aa54868
강혜미 <eham1003@naver.com>
2023-07-25 23:52:48 +0900

    deque-2164:카드

---
+++ data_structure/queue/2164-카드2.py
@@ -0,0 +1,13 @@
+from _collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+deque = deque([i+1 for i in range(N)])
+
+while (len(deque) != 1):
+    deque.popleft()
+    item = deque.popleft()
+    deque.append(item)
+
+print(deque[0])
