0063f5556c08b32319080240dc2be1275a72accb
강혜미 <eham1003@naver.com>
2023-08-21 12:07:28 +0900

    구현 3

--- data_structure/구현/implementation.py
+++ data_structure/구현/implementation.py
@@ -104,8 +104,8 @@
 
 ## 방법 1.
 
-N = int(input())
-num = list(input().split())
+# N = int(input())
+# num = list(input().split())
 
 # def digit_sum(x):
 #     plus = 0
@@ -124,21 +124,138 @@ num = list(input().split())
 
 
 ## 방법 2.
-def digit_sum(x):
-    sum = 0
-    while x > 0:
-        sum += x % 10
-        x //= 10
-    return sum
+# def digit_sum(x):
+#     sum = 0
+#     while x > 0:
+#         sum += x % 10
+#         x //= 10
+#     return sum
+#
+# max = 0
+# for i in num:
+#     total = digit_sum(i)
+#     if total > max:
+#         max = total
+#         res = i
+#
+# print(res)
 
-max = 0
-for i in num:
-    total = digit_sum(i)
-    if total > max:
-        max = total
-        res = i
 
-print(res)
+# 7. 소수 구하기
 
 
+## 에라토스테네스 체
+# x = 200000
+# array = [True for _ in range(x+1)]
+#
+# for i in range(2, int((x ** 0.5)) + 1):
+#     if array[i] == True:
+#         j = 2
+#         while i * j <= x:
+#             array[i*j] = False
+#             j += 1
+#
+#
+# N = int(input())
+# cnt = 0
+# for i in range(2, N + 1):
+#     if array[i] == True:
+#         print(i)
+#         cnt += 1
+# print(cnt)
 
+# 8. 뒤집은 소수
+# n = 100000
+# array = [True for _ in range(n+1)]
+# for i in range(2, int(n ** 0.5) + 1):
+#     if array[i] == True:
+#         j = 2
+#         while i * j <= n:
+#             array[i * j] = False
+#             j += 1
+#
+# def isPrime(x):
+#     if x == 1:
+#         return False
+#     elif array[x] == True:
+#         return True
+#     else:
+#         return False
+#
+# def reverse(x):
+#     res = 0
+#     while x > 0:
+#         t = x % 10
+#         res = res * 10 + t
+#         x //= 10
+#     return res
+#
+# N = int(input())
+# num = list(map(int, input().split()))
+# for i in num:
+#     x = reverse(i)
+#     if isPrime(x):
+#         print(x, end=" ")
+
+# 9. 주사위 게임
+# import sys
+# N = int(input())
+# price = list()
+# max = 0
+# for i in range(N):
+#     num = sys.stdin.readline().split()
+#     num.sort()
+#     a, b, c = map(int, num)
+#     cnt = 0
+#
+#     if a== b and b == c:
+#         money =10000 + a * 1000
+#     elif a == b or  a == c:
+#         money = 1000 + a * 100
+#     elif b == c:
+#         money = 1000 + b * 100
+#     else:
+#         money = c*100
+#
+#     if money > max:
+#         max = money
+#
+# print(max)
+
+# 10. 점수 계산
+# N = int(input())
+# answer = list(map(int, input().split()))
+# score = [0 for _ in range(N)]
+# sum = 0
+#
+# for idx, j in enumerate(answer):
+#     if idx == 0:
+#         if j == 0:
+#             score[idx] = 0
+#         else:
+#             score[idx] = 1
+#             sum += 1
+#     else:
+#         if j == 0:
+#             score[idx] = 0
+#         else:
+#             if score[idx -1] > 0:
+#                 score[idx] = score[idx -1] + 1
+#             else:
+#                 score[idx] = 1
+#             sum += score[idx]
+#
+# print(sum)
+
+## 다른 방법
+N = int(input())
+answer = list(map(int, input().split()))
+sum = 0
+cnt = 0
+
+for x in answer:
+    if x == 1:
+        cnt += 1
+        sum += cnt
+    else:
+        cnt = 0
